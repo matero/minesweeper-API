067ea8d593cdeca092bea7731dd77d5295209ceb
Juan J. Gil <matero@gmail.com>
2020-11-14 09:54:36 -0300

    documents and simplify builder methods.

--- src/main/java/minesweeper/Board.java
+++ src/main/java/minesweeper/Board.java
@@ -2,6 +2,12 @@ package minesweeper;
 
 import java.util.Random;
 
+/**
+ * A {@link Board} instance represents the cells in a game.
+ * <p>
+ * {@link Board} instances are immutable, and knows where the mines are located. They allows to query if a cell has a
+ * mine with {@link Board#hasMine(int, int)} and to fetch the amount of mines surrounding a cell with {@link Board#sorroundingMines(int, int)}.
+ */
 class Board
 {
   private static final int MINE = Integer.MIN_VALUE;
@@ -12,6 +18,40 @@ class Board
   {
     // we don't need to clone the array because its always comes from a factory method that ensures its not shared.
     this.cells = cells;
+  }
+
+  int sorroundingMines(final int row, final int column)
+  {
+    if (row < 0) {
+      throw new IllegalArgumentException("row must be 0 or positive");
+    }
+    if (row >= cells.length) {
+      throw new IllegalArgumentException("row is too big. This board has " + cells.length + " rows (and this board access is 0..n-1 indexed)");
+    }
+    if (column < 0) {
+      throw new IllegalArgumentException("column must be 0 or positive");
+    }
+    if (column >= cells[0].length) {
+      throw new IllegalArgumentException("row is too big. This board has " + cells[0].length + " column (and this board access is 0..n-1 indexed)");
+    }
+    return (cells[row][column] == MINE)? -1 : cells[row][column];
+  }
+
+  boolean hasMine(final int row, final int column)
+  {
+    if (row < 0) {
+      throw new IllegalArgumentException("row must be 0 or positive");
+    }
+    if (row >= cells.length) {
+      throw new IllegalArgumentException("row is too big. This board has " + cells.length + " rows (and this board access is 0..n-1 indexed)");
+    }
+    if (column < 0) {
+      throw new IllegalArgumentException("column must be 0 or positive");
+    }
+    if (column >= cells[0].length) {
+      throw new IllegalArgumentException("row is too big. This board has " + cells[0].length + " column (and this board access is 0..n-1 indexed)");
+    }
+    return cells[row][column] == MINE;
   }
 
   static Board easy() { return new Board(createCells(Level.EASY)); }
@@ -45,43 +85,46 @@ class Board
   static int[][] createCells(final int rows, final int columns, final int mines)
   {
     final var board = new int[rows][columns];
-    randomlyPlaceMinesAt(board, rows, columns, mines);
-    calculateSurroundingMinesAt(board, rows, columns);
+    randomlyPlaceMinesAt(board, mines);
+    calculateSurroundingMinesAt(board);
     return board;
   }
 
-  static void randomlyPlaceMinesAt(final int cells[][], final int rows, final int columns, final int mines)
+  static void randomlyPlaceMinesAt(final int[][] cells, final int mines)
   {
-    {
-      final var r = new Random();
-      for (int minesToPlace = mines; minesToPlace > 0; ) {
-        final int row = r.nextInt(rows);
-        final int column = r.nextInt(columns);
-        final var cell = cells[row][column];
+    final var r = new Random();
+    final int rows = cells.length;
+    final int columns = cells[0].length;
+    for (int minesToPlace = mines; minesToPlace > 0; ) {
+      final int row = r.nextInt(rows);
+      final int column = r.nextInt(columns);
+      final var cell = cells[row][column];
 
-        if (cell == 0) {
-          cells[row][column] = MINE;
-          minesToPlace--;
-        }
+      if (cell == 0) {
+        cells[row][column] = MINE;
+        minesToPlace--;
       }
     }
   }
 
-  static void calculateSurroundingMinesAt(final int[][] cells, final int rows, final int columns)
+  static void calculateSurroundingMinesAt(final int[][] cells)
   {
+    final int rows = cells.length;
+    final int columns = cells[0].length;
+
     for (int row = 0; row < rows; row++) {
       for (int column = 0; column < columns; column++) {
         if (!mineAt(cells, row, column)) {
-          final var isNotAtLastRow = (row + 1) != rows;
-          final var isNotAtLastColumn = (column + 1) != columns;
-          cells[row][column] = surroundingMinesOf(cells, row, isNotAtLastRow, column, isNotAtLastColumn);
+          cells[row][column] = surroundingMinesOf(cells, row, column);
         }
       }
     }
   }
 
-  private static int surroundingMinesOf(final int[][] cells, final int row, final boolean isNotAtLastRow, final int column, final boolean isNotAtLastColumn)
+  private static int surroundingMinesOf(final int[][] cells, final int row, final int column)
   {
+    final var isNotAtLastRow = (row + 1) != cells.length;
+    final var isNotAtLastColumn = (column + 1) != cells[0].length;
     int surroundingMines = 0;
 
     if (isNotAtFirst(column)) {
